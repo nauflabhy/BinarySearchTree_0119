d703af71741a0cfb61fa68b4474aacf91584bf6c
nauflabhy <n4uf4l.ku@gmail.com>
2025-06-26 15:30:57 +0700

    fungsi inorder

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -98,4 +98,19 @@ class BinaryTree
         // Check if the tree is empty
         return ROOT == nullptr;
     }
+
+    void inorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+            return;
+
+        inorder(ptr->leftchild);
+        cout << ptr->info << " "; //parent
+        inorder(ptr->rightchild);
+    }
 };
